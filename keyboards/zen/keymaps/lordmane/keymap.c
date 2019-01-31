3adb9e901fe2801ff4a5f97a326f3067d4a567a8
Manuel Fco. del Amor <mfdelamor@externos.itnow.es>
2019-01-31 17:20:41 +0100

    New Enter key. Left control+caps

--- keyboards/zen/keymaps/lordmane/keymap.c
+++ keyboards/zen/keymaps/lordmane/keymap.c
@@ -45,6 +45,7 @@ enum keycodes {
 #define LT_TAB LT(_LOWER, KC_TAB)
 #define LT_CAPS LT(_LOWER, KC_CAPSLOCK )
 #define LS_DEL LSFT_T(KC_DEL)
+#define LC_CAPS LCTL_T(KC_CAPS)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -54,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LBRC, KC_Q,      KC_W,    KC_F,   KC_P,   AGT(KC_B),                AGT(KC_J),   KC_L,    KC_U,    KC_Y,   KC_SCLN,   KC_RBRC, \
     KC_GRV,  LST(KC_A), KC_R,    KC_S,   KC_T,   KC_G,                     KC_M,        KC_N,    KC_E,    KC_I,   RST(KC_O), KC_QUOT, \
     KC_MINS, KC_Z,      KC_X,    KC_C,   KC_D,   KC_V,                     KC_K,        KC_H,    KC_COMM, KC_DOT, KC_SLSH,   KC_EQL,  \
-    KC_LCTL, KC_LGUI,   KC_LALT, KC_INS, KC_ESC, LST(KC_DEL),LT_TAB,M_BSPC,LT_SPC,      LT_CAPS, KC_RALT, KC_RGUI,  KC_RCTL,   KC_BSLS  \
+    LC_CAPS, KC_LGUI,   KC_LALT, KC_INS, KC_ESC, LST(KC_DEL),LT_TAB,M_BSPC,LT_SPC,      KC_ENT,  KC_RALT, KC_RGUI,KC_RCTL,   KC_BSLS  \
   ),
 
   [_QWERTY] = LAYOUT( \
